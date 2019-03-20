f42deb67fb72a3cac5d7f6014acfb72416b739b3
Tyler Long <tyler4long@gmail.com>
2019-03-20 21:34:24 +0800

    Initial commit

---
+++ q1a1.js
@@ -0,0 +1,21 @@
+
+const t = 'abcde'
+const s1 = 'ace'
+const s2 = 'aec'
+
+const isSubString = (s, t) => {
+  if(s.length === 0) {
+    return true
+  }
+  if(t.length < s.length) {
+    return false
+  }
+  const firstCharacterIndex = t.indexOf(s[0])
+  if(firstCharacterIndex === -1) {
+    return false
+  }
+  return isSubString(s.substring(1), t.substring(firstCharacterIndex + 1))
+}
+
+console.log(isSubString(s1, t))
+console.log(isSubString(s2, t))
